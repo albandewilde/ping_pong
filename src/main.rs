f6abf0bb0c8cdabf0de0f1a39b9733cfab3a1a9a
albandewilde <dewilde@intechinfo.fr>
2020-03-22 21:31:45 +0100

    Write the basics functions I'll need

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+
+fn there_in_a_winner() -> bool {
+    return rand::thread_rng().gen_range(0, 5) == 0;
+}
+
+fn the_bot_win() -> bool {
+    return rand::thread_rng().gen_range(0, 3) == 0;
+}
+
+fn results(ask: &str) -> String {
+    let pp = ask.to_lowercase();
+    if there_in_a_winner() && (pp == "ping" || pp == "pong") {
+        let mut win = String::from("There is a winner.\n");
+        if the_bot_win() {
+            win.push_str("I won !");
+        } else {
+            win.push_str("You won, congrats.");
+        }
+        win
+    } else {
+        String::from(
+            if pp == "ping" {
+                "pong"
+            } else if pp == "pong" {
+                "ping"
+            } else {
+                "..."
+            }
+        )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", results("plop"));
 }
