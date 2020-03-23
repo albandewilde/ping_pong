3fae07bc7c07d7c20cca8fa0c057e78962a2a1bf
albandewilde <dewilde@intechinfo.fr>
2020-03-23 10:08:49 +0100

    Write the bot

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 use rand::Rng;
+use std::env;
+
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
 
 fn there_in_a_winner() -> bool {
     return rand::thread_rng().gen_range(0, 5) == 0;
@@ -10,16 +16,14 @@ fn the_bot_win() -> bool {
 
 fn results(ask: &str) -> String {
     let pp = ask.to_lowercase();
-    if there_in_a_winner() && (pp == "ping" || pp == "pong") {
-        let mut win = String::from("There is a winner.\n");
-        if the_bot_win() {
-            win.push_str("I won !");
+    String::from(
+        if there_in_a_winner() && (pp == "ping" || pp == "pong") {
+            if the_bot_win() {
+                "You miss the ball.\nI won !"
+            } else {
+                "I miss the ball.\nYou won, congrats."
+            }
         } else {
-            win.push_str("You won, congrats.");
-        }
-        win
-    } else {
-        String::from(
             if pp == "ping" {
                 "pong"
             } else if pp == "pong" {
@@ -27,10 +31,36 @@ fn results(ask: &str) -> String {
             } else {
                 "..."
             }
-        )
+        }
+    )
+}
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.content.chars().nth(0).unwrap() == '¨' {
+
+            // 2 because ¨ is a two bites caracter
+            let result = results(&msg.content.get(2..).unwrap());
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, result) {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is ready to match !", ready.user.name);
     }
 }
 
 fn main() {
-    println!("{}", results("plop"));
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+    let mut client = Client::new(&token, Handler).expect("Err creating client");
+
+    if let Err(why) = client.start_shards(2) {
+        println!("Client error: {:?}", why);
+    }
 }
